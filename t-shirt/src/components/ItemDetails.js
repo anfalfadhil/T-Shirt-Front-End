c08ae877f1ee252edd4824aa0c37b04051b77066
anfalfadhil <87868348+anfalfadhil@users.noreply.github.com>
2021-09-24 16:01:38 -0400

    adding deleting functionality is done

--- t-shirt/src/components/ItemDetails.js
+++ t-shirt/src/components/ItemDetails.js
@@ -6,6 +6,7 @@ import axios from "axios";
 function ItemDetails() {
   const [img, setImg] = useState({});
   const { imageid } = useParams();
+  const [deleted, setDeleted] = useState(false);
 
   useEffect(() => {
     axios
@@ -19,11 +20,22 @@ function ItemDetails() {
       });
   }, []);
 
+  const onDeleteHandeler = (event) => {
+    const url = `${APIURL}/items/${match.params.id}`;
+
+    fetch(url, { method: "DELETE" })
+      .then((res) => {
+        setDeleted(true);
+      })
+      .catch(console.error);
+  };
+
   return (
     <div className="item-details-container">
       <h2 className="title">{img.title}</h2>
       <img className="chosen-item" src={img.image} />
-      <button type="buy">Buy</button>
+      <button>Buy</button>
+      <button onClick={onDeleteHandeler}>DELETE</button>
     </div>
   );
 }
