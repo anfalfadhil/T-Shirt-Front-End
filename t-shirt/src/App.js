947ad947e3d910ae1cb78606928b3f9543628b6a
Vanessa Su <vnessa.su@gmail.com>
2021-09-24 14:08:10 -0400

    :hammer: Add react router dom

--- t-shirt/src/App.js
+++ t-shirt/src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Welcome to T-Shirt</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1> Welcome to T-Shirt</h1>
+            </header>
+            <Switch></Switch>
+        </div>
+    );
 }
 
 export default App;
