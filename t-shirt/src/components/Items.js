1b4e1cccf717ee465508bdfe1d6bfe44d040041a
Vanessa Su <vnessa.su@gmail.com>
2021-09-27 10:05:58 -0400

    :fire: Remove excess code

--- t-shirt/src/components/Items.js
+++ t-shirt/src/components/Items.js
@@ -4,43 +4,33 @@ import { Link } from "react-router-dom";
 import { APIURL } from "../config.js";
 
 function Items() {
-  const [data, setData] = useState([]);
-  // const [imageId, setImageId] = useState();
-  // const api_key = process.env.REACT_APP_API_KEY;
+    const [data, setData] = useState([]);
 
-  useEffect(() =>
-  {
-    axios.get(`${APIURL}/items`)
-        .then( (response) =>
-        {
-          setData(response.data);
-        })
-        .catch(function (error)
-        {
-          console.log(error);
-        });
-      
-  }, []);
+    useEffect(() => {
+        axios
+            .get(`${APIURL}/items`)
+            .then((response) => {
+                setData(response.data);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }, []);
 
-  return (
-    <div className="item-container">
-      {data.map( (item) =>
-      {
-        return (
-          
-            <div className="card" key={item._id}>
-              {/* <img src={e.baseimageurl} alt=""/> */}
-              <Link to={`/items/${item._id}`}> 
-                <h2> {item.name} </h2>
-                <h4> {item.description} </h4>
-              </Link>
-              
-            </div>
-          
-        );
-      })}
-    </div>
-  );
+    return (
+        <div className="item-container">
+            {data.map((item) => {
+                return (
+                    <div className="card" key={item._id}>
+                        <Link to={`/items/${item._id}`}>
+                            <h2> {item.name} </h2>
+                            <h4> {item.description} </h4>
+                        </Link>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default Items;
