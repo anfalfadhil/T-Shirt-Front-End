58e67423fa6de81a740830d3b20bfcc07889e717
Vanessa Su <vnessa.su@gmail.com>
2021-09-26 23:51:49 -0400

    :hammer: Add item edit form component

--- t-shirt/src/App.js
+++ t-shirt/src/App.js
@@ -1,19 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { Link, Route, Switch } from "react-router-dom";
-import { Items } from "./components/Items";
+import Items from "./components/Items";
 import ItemDetails from "./components/ItemDetails";
+import EditItemForm from "./components/EditItemForm";
 
 function App() {
-  return (
-    <div className="App">
-      <h1> Welcome to T-Shirt</h1>
-      <Route path="/" exact component={Items} />
-      <Route path="/item/:itemid" exact>
-        <ItemDetails />
-      </Route>
-      <Switch></Switch>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1> Welcome to T-Shirt</h1>
+            <Route path="/" exact component={Items} />
+            <Route path="/item/:itemid" exact>
+                <ItemDetails />
+            </Route>
+            <Switch>
+                <Route path="/item/:itemId/edit" component={EditItemForm} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
