1e95606589978cc5d162a15edc8027c272bd7479
anfalfadhil <87868348+anfalfadhil@users.noreply.github.com>
2021-09-27 13:41:08 -0400

    fetch is working

--- t-shirt/src/components/ItemDetails.js
+++ t-shirt/src/components/ItemDetails.js
@@ -6,58 +6,58 @@ import { APIURL } from "../config.js";
 import Button from "react-bootstrap/Button";
 
 function ItemDetails({ match }) {
-    const [deleted, setDeleted] = useState(false);
-    const [item, setItem] = useState({});
-    const [editing, setEditing] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+  const [item, setItem] = useState({});
+  const [editing, setEditing] = useState(false);
 
-    useEffect(() => {
-        axios
-            .get(`${APIURL}/items/${match.params.id}`)
-            .then((response) => {
-                setItem(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }, [match.params.id]);
+  useEffect(() => {
+    axios
+      .get(`${APIURL}/items/${match.params.id}`)
+      .then((response) => {
+        setItem(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [match.params.id]);
 
-    const deleteItem = (event) => {
-        const url = `${APIURL}/items/${match.params.id}`;
+  const deleteItem = (event) => {
+    const url = `${APIURL}/items/${match.params.id}`;
 
-        axios
-            .delete(url)
-            .then((response) => {
-                setDeleted(true);
-            })
-            .catch(console.error);
-    };
+    axios
+      .delete(url)
+      .then((response) => {
+        setDeleted(true);
+      })
+      .catch(console.error);
+  };
 
-    const editItem = () => {
-        setEditing(true);
-    };
+  const editItem = () => {
+    setEditing(true);
+  };
 
-    if (deleted) {
-        return <Redirect to="/" />;
-    }
+  if (deleted) {
+    return <Redirect to="/" />;
+  }
 
-    if (editing) {
-        return <Redirect to={`/items/${match.params.id}/edit`} />;
-    }
+  if (editing) {
+    return <Redirect to={`/items/${match.params.id}/edit`} />;
+  }
 
-    return (
-        <div className="item-details-container">
-            <h2> {item.name} </h2>
-            <h3> {item.description} </h3>
-            <h3> {item.price} </h3>
+  return (
+    <div className="item-details-container">
+      <h2> {item.name} </h2>
+      <h3> {item.description} </h3>
+      <h3> {item.price} </h3>
 
-            <Button variant="secondary" onClick={deleteItem}>
-                Buy
-            </Button>
-            <Button variant="secondary" onClick={editItem}>
-                Edit
-            </Button>
-        </div>
-    );
+      <Button variant="secondary" onClick={deleteItem}>
+        Buy
+      </Button>
+      <Button variant="secondary" onClick={editItem}>
+        Edit
+      </Button>
+    </div>
+  );
 }
 
 export default ItemDetails;
