92e1c0e1c87b72ff69a491f3ebba4d2d19f1eaea
Vanessa Su <vnessa.su@gmail.com>
2021-09-27 17:23:57 -0400

    :construction: WIP order list uses seeded data to populate list

--- t-shirt/src/components/OrderList.js
+++ t-shirt/src/components/OrderList.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { APIURL } from "../config";
 
-function OrderList({ items }) {
+function OrderList() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const url = `${APIURL}/order`;
+
+        axios
+            .get(url)
+            .then((res) => {
+                console.log(res.data[0].items[0].seedOrderItems);
+                setData(res.data[0].items[0].seedOrderItems);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
     return (
         <div className="order-container">
-            {items.map((item) => (
+            {data.map((item) => (
                 <div key={item._id} className="order-item">
                     <Link to={`/item/${item._id}`}>{item.name}</Link>
                     <p>${item.price}</p>
