bcbaf037e4cf6b74d5d784a322cafe91523b1abc
Dimitri Lafleur <Dimitri@Dimitris-MacBook-Pro.local>
2021-09-28 12:04:36 -0400

    Bitcoin QR code component - typo

--- t-shirt/src/components/Bitcoin.js
+++ t-shirt/src/components/Bitcoin.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-// Only work with Bitcoin Cash  - BCH (Not Bitcoin) - learn  more @ https://bitcoincash.org/
+// Only works with Bitcoin Cash  - BCH (Not Bitcoin) - learn  more @ https://bitcoincash.org/
 
 //payment data will be here
 //https://blockchair.com/search?q=qpuyrhda7zrn3jk6yzr98zxq3a6uavvqkspgv5fdmu
