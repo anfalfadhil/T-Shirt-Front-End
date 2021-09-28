f19db4b0e8e4a3303cd945d1f1fb06f30342587e
Dimitri Lafleur <Dimitri@Dimitris-MacBook-Pro.local>
2021-09-28 11:25:30 -0400

    Bitcoin QR code component - data

--- t-shirt/src/components/Bitcoin.js
+++ t-shirt/src/components/Bitcoin.js
@@ -2,6 +2,9 @@ import React from "react";
 
 // Only work with Bitcoin Cash  - BCH (Not Bitcoin) - learn  more @ https://bitcoincash.org/
 
+//payment data will be here
+//https://blockchair.com/search?q=qpuyrhda7zrn3jk6yzr98zxq3a6uavvqkspgv5fdmu
+
 const Bitcoin = () => {
   return (
     <div>
