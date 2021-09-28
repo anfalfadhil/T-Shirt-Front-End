c3da6004a8179027bfe0e9671cebf5a4e1d1ae7f
anfalfadhil <87868348+anfalfadhil@users.noreply.github.com>
2021-09-28 13:27:26 -0400

    testin

--- t-shirt/src/components/Items.js
+++ t-shirt/src/components/Items.js
@@ -5,34 +5,36 @@ import { APIURL } from "../config.js";
 import SearchBar from "./SearchBar.js";
 
 function Items() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        axios
-            .get(`${APIURL}/items`)
-            .then((response) => {
-                setData(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${APIURL}/items`)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-    return (
-        <div className="item-container">
-            <SearchBar onSubmit={setData} />
-            {data.map((item) => {
-                return (
-                    <div className="card" key={item._id}>
-                        <Link to={`/items/${item._id}`}>
-                            <h2> {item.name} </h2>
-                            <h4> {item.description} </h4>
-                        </Link>
-                    </div>
-                );
-            })}
-        </div>
-    );
+  return (
+    <div className="item-container">
+      <SearchBar onSubmit={setData} />
+      {console.log(data)};
+      {data.map((item) => {
+        return (
+          <div className="card" key={item._id}>
+            <Link to={`/items/${item._id}`}>
+              <h2> {item.name} </h2>
+              <h2>${item.price}</h2>
+              <img src={item.image} />
+            </Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Items;
