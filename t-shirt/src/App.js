cebb16f7dc32fe2b51fa92323244ffdb9989684a
Ronny Acevedo <ronny.acv2@hotmail.com>
2021-09-29 10:20:21 -0400

    :fire: removed old dummy data

--- t-shirt/src/App.js
+++ t-shirt/src/App.js
@@ -7,37 +7,6 @@ import EditItemForm from "./components/EditItemForm";
 import OrderList from "./components/OrderList";
 import PostNewItem from "./components/PostNewItem";
 
-const dummyOrderData = [
-    {
-        name: "Sofa",
-        description:
-            "Scandinavian Helsinki irure, airport Melbourne cillum dolore duis. Elegant elit smart, Asia-Pacific airport Nordic aute exclusive velit Beams uniforms ex Shinkansen. Ut wardrobe id, anim Winkreative non uniforms occaecat bespoke Helsinki.",
-        price: 100,
-        _id: 1,
-    },
-    {
-        name: "Bicycle",
-        description:
-            "Intricate tempor laboris essential Fast Lane sint sophisticated Swiss bulletin eiusmod anim in. Airport soft power Comme des Garçons signature Winkreative nisi labore wardrobe, velit Marylebone proident dolore.",
-        price: 80,
-        _id: 2,
-    },
-    {
-        name: "Office Chair",
-        description:
-            "Scandinavian Helsinki irure, airport Melbourne cillum dolore duis. Elegant elit smart, Asia-Pacific airport Nordic aute exclusive velit Beams uniforms ex Shinkansen. Ut wardrobe id, anim Winkreative non uniforms occaecat bespoke Helsinki.",
-        price: 35,
-        _id: 3,
-    },
-    {
-        name: "6 Sets of Dishes and Cutlery",
-        description:
-            "K-pop ad handsome sleepy pintxos nulla Porter voluptate delightful consequat quality of life. Elegant tote bag ut exclusive finest Ginza dolor et Scandinavian. Boulevard in excepteur, exquisite Muji dolor boutique laborum Winkreative dolore laboris Fast Lane.",
-        price: 20,
-        _id: 4,
-    },
-];
-
 function App() {
     return (
         <div className="App">
@@ -51,9 +20,7 @@ function App() {
                 <Route path="/items/new" component={PostNewItem} />
                 <Route path="/items/:id" exact component={ItemDetails} />
                 <Route path="/items/:id/edit" component={EditItemForm} />
-                <Route path="/order">
-                    <OrderList items={dummyOrderData} />
-                </Route>
+                <Route path="/order" component={OrderList} />
             </Switch>
         </div>
     );
